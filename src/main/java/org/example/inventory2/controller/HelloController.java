47132717cf5e67873255d80b93a06ac7134f02bc
Andreas Beilharz <andreas.beilharz@gmx.net>
2025-12-31 13:06:26 +0100

    Add buildspec.yml and the Dockerfile

--- src/main/java/org/example/inventory2/controller/HelloController.java
+++ src/main/java/org/example/inventory2/controller/HelloController.java
@@ -1,10 +1,18 @@
 package org.example.inventory2.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> healthcheck() {
+        return ResponseEntity.ok().body(Map.of("Status", "Up"));
+    }
 
     @GetMapping("/hello")
     public String hello() {
